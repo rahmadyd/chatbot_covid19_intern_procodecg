c666bd21eca2901af2efde9f61a825d0b5d46e5e
Rahmad Hidayad <69381544+rahmadyd@users.noreply.github.com>
2025-10-19 19:26:49 +0700

    mau siap siap upload ke github

--- src/retriever.py
+++ src/retriever.py
@@ -1,1 +1,13 @@
  # Script retrieval dari vector DB
+ 
+import faiss
+import json
+import sentence_transformers
+import transformer
+import torch
+import config
+from config import MODEL_CONFIG, INDEX_PATH, TEXT_PATH
+
+index = faiss.read_index(INDEX_PATH)
+
+
